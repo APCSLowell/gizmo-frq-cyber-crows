9be75c4baf00cfac63619af7aebe499dcf916474
cybercrow <malcolmbarrykao@gmail.com>
2025-04-13 23:49:33 -0700

    Update Gizmo.java

--- src/main/java/Gizmo.java
+++ src/main/java/Gizmo.java
@@ -11,6 +11,25 @@ public class Gizmo
   /** Returns true if this Gizmo is electronic, and false
   * otherwise.
   */
+
+  public int countElectronicsByMaker(String maker){
+  int count=0; 
+    for(int i=0;i<purchases.size();i++){
+      if(purchases.getMaker().equals(maker) && purchases.isElectronic())
+        count++; 
+    }
+  return count; 
+  
+  public boolean hasAdajacentEqualPair(){
+    if(purchases.size()<2)
+      return true; 
+    for(int i=0; i<purchases.size()-1;i++){
+      if(purchases.get(i).equals(purchases.get(i+1)))
+        return true; 
+    }
+    return false; 
+    
+  }
   public boolean isElectronic()
   {
     return isElec;
